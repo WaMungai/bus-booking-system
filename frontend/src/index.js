e45fb3e9af77f4eaf175dbb97f62c309a745f8e9
The Passionate Programmer <wamungai10@gmail.com>
2024-04-12 14:33:56 +0300

    Fix dependancies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import './index.css'; // Importing any global styles
+import App from './App'; // Importing the root component of the application
+import reportWebVitals from './reportWebVitals'; // Importing web vitals reporting (optional)
+import { BrowserRouter } from 'react-router-dom'; // Importing BrowserRouter for routing (if used)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Rendering the root component of the application into the DOM
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-    
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root') // Mounting point in the HTML file
 );
 
-// If you want to start measuring performance in your app, pass a function
+// Optional: If you want to measure performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
